b2676befea90e9a12e2be3a4d597d1559e3429a3
Donya <thisis2nya@gmail.com>
2023-08-06 21:18:15 +0330

    [UI] create buttons

--- components/TheCalculator.vue
+++ components/TheCalculator.vue
@@ -1,5 +1,117 @@
 <template>
   <div
-    class="w-[22rem] h-[44rem] bg-gradient-to-b from-neutral-700 to-neutral-900 shadow-xl shadow-neutral-950 rounded-3xl flex flex-col items-start justify-between"
-  ></div>
+    class="w-[21rem] h-[44rem] p-4 bg-gradient-to-b from-neutral-700 to-neutral-900 shadow-xl shadow-neutral-950 rounded-3xl flex flex-col items-center justify-between"
+  >
+    <span></span>
+
+    <!-- buttons -->
+    <div class="w-full flex flex-col gap-3.5">
+      <!-- normal -->
+      <div class="w-full flex justify-end gap-3.5">
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          0
+        </button>
+      </div>
+      <!-- solid orange & purple -->
+      <div class="w-full flex justify-between gap-3.5">
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          1
+        </button>
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          2
+        </button>
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          3
+        </button>
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          4
+        </button>
+      </div>
+      <!-- light -->
+      <div class="w-full flex justify-between gap-3.5">
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          1
+        </button>
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          2
+        </button>
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          3
+        </button>
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          4
+        </button>
+      </div>
+      <!-- orange -->
+      <div class="w-full flex justify-between gap-3.5">
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          1
+        </button>
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          2
+        </button>
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          3
+        </button>
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          4
+        </button>
+      </div>
+      <!-- purple -->
+      <div class="w-full flex justify-between gap-3.5">
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          1
+        </button>
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          2
+        </button>
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          3
+        </button>
+        <button
+          class="button w-16 h-14 rounded-full text-zinc-500 bg-neutral-800 focus:outline-none"
+        >
+          4
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+.button {
+  box-shadow: 1px 1px 4px black, -1px -1px 3px #4e4e4e;
+}
+</style>

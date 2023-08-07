24dcf5e4876e068d0517d6328f62b71e81b7c9c9
Donya <thisis2nya@gmail.com>
2023-08-07 08:42:49 +0330

    [fix] components' name mistakes

--- components/TheCalculator.vue
+++ components/TheCalculator.vue
@@ -15,6 +15,6 @@
 
 <script>
 export default {
-  name: " TheCalculator",
+  name: "TheCalculator",
 };
 </script>
--- components/TheOperation.vue
+++ components/TheOperation.vue
@@ -4,6 +4,6 @@
 
 <script>
 export default {
-  name: " TheOperation",
+  name: "TheOperation",
 };
 </script>
